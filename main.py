87791aef36989646be50be2777f16f6d64dd36f0
juancobo6 <100451216@alumnos.uc3m.es>
2024-11-03 17:23:57 +0100

    class Brainnote implemented

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import json
 from itertools import islice
 
-
-def parse_command(user_input):
-    if user_input[0] == "c":
-        print("New chapter")
-    elif user_input[0] == "s":
-        print("New section")
+NOTES_PATH = r"C:\Users\JuanC\Documents\Projectos\brainnotes\notes"
 
 
-def parse(user_input):
-    if user_input[0] == ".":
-        parse_command(user_input[1:])
-    else:
-        print(user_input)
+class Brainnote:
+    def __init__(self, note, path):
+        self.note = note
+        self.path = path
+        self.index = self.get_index()
+
+
+    def get_index(self):
+        if not self.note["content"]:
+            return 0
+        else:
+            return self.note["content"][-1]["index"] + 1
+
+
+    def write_note(self):
+        while True:
+            user_input = input("> ")
+            if user_input == "":
+                continue
+            elif user_input[0:2] == (".1" or ".2" or ".3"):
+                self.parse_title(user_input)
+            elif user_input[0:2] == ".e":
+                break
+            else:
+                self.parse_paragraph(user_input)
+        print("Saving note...")
+        with open(self.path, 'w') as f:
+            json.dump(self.note, f, indent=4)
+
+
+    def parse_title(self, user_input):
+        title = user_input[3:]
+        grade = int(user_input[1])
+        self.note["content"].append({"index": self.index, "type": str(grade), "content": title, "summary": ""})
+        self.index += 1
+
+
+    def parse_paragraph(self, user_input):
+        self.note["content"].append({"index": self.index, "type": "4", "content": user_input, "revision": ""})
+        self.index += 1
 
 
 def intro():
@@ -24,27 +54,40 @@ def intro():
     for note in notes:
         print(f"{index}. {note}")
         index += 1
+    print("n. New note")
 
-    note_index = int(input("Enter the note number: ")) - 1
+    note_index = input("Enter the note number: ")
 
-    note_title, note_path = next(islice(notes.items(), note_index, note_index + 1))
+    if note_index == "n":
+        note_title = input("Enter the note title: ")
+        note_context = input("Enter the note context: ")
+        notes[note_title] = f"{NOTES_PATH}\\{note_title}.json"
+        with open("notes.json", "w") as f:
+            json.dump(notes, f, indent=4)
+        new_note = {"title": note_title, "context": note_context, "content": []}
+        with open(f"{NOTES_PATH}\\{note_title}.json", "w") as f:
+            json.dump(new_note, f, indent=4)
+        return {"title": note_title, "content": []}, f"{NOTES_PATH}\\{note_title}.json"
 
-    print(f"Opening {note_title}...")
-    with open(note_path) as f:
-        note = json.load(f)
+    else:
+        note_index = int(note_index) - 1
+        note_title, note_path = next(islice(notes.items(), note_index, note_index + 1))
 
-    return note
+        print(f"Opening {note_title}...")
+        with open(note_path) as f:
+            note = json.load(f)
+
+        return note, note_path
 
 
 def main():
     print("Welcome to Brain notes!")
 
-    note = intro()
+    note, path = intro()
 
-    while True:
-        # Take user input and print it
-        user_input = input()
-        parse(user_input)
+    bn = Brainnote(note, path)
+
+    bn.write_note()
 
 
 if __name__ == "__main__":
